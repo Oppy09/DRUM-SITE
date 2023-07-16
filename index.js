865b5e53d34798fbc771d97773b11784619ca8ae
Aderinola Opemipo Emmanuel <aderinolaopemipo@gmail.com>
2023-07-16 12:55:56 +0100

    .

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ function buttonAnimation(currentKey) {
     activeButton.classList.add("pressed");
 
     setTimeout(function() {
-        activeButton.classList.remove("pressed")
+        activeButton.classList.remove("pressed");
     }, 100);
 }
